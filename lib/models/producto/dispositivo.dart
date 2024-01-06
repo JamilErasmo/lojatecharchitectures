11c7dac398781619850ffa639b7b52a3a792eb4d
Jamil88 <jamilerasmoq@gmail.com>
2024-01-06 16:40:41 -0500

    caprpeta producto

---
+++ lib/models/producto/dispositivo.dart
@@ -0,0 +1,12 @@
+class Dispositivo {
+  String dispositivoCalidad;
+  String tipoDispositivo;
+
+  Dispositivo({
+    required this.dispositivoCalidad,
+    required this.tipoDispositivo,
+  });
+
+  void realizarPruebaCalidad() {}
+  void enviarNotificacion() {}
+}
---
+++ lib/models/producto/medicamento.dart
@@ -0,0 +1,15 @@
+class Medicamento {
+  String contradicciones;
+  String indicaciones;
+  String tipoMedicamento;
+
+  Medicamento({
+    required this.contradicciones,
+    required this.indicaciones,
+    required this.tipoMedicamento,
+  });
+
+  // Métodos de la clase Medicamento
+//  void verificarContradicciones() {}
+//  void proporcionarIndicaciones() {}
+}
